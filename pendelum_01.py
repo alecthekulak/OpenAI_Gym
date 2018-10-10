f454c7720022b87ccc81b7de4983bf50ee7f33d2
Alec Kulakowski <alecthekulak@gmail.com>
2018-10-10 11:10:55 -0400

    still linear SVM
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- pendelum_01.py
+++ pendelum_01.py
@@ -3,11 +3,15 @@ import gym
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import SGDRegressor, Perceptron
+from sklearn.linear_model import SGDRegressor
+from sklearn.svm import SVR
+from sklearn.neural_network import MLPRegressor
 
 env = gym.make('Pendulum-v0')
-model = SGDRegressor(tol=None, max_iter=None)
-# model = Perceptron()
+# model = SGDRegressor(tol=None, max_iter=None)  # 3.53%
+# model = MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam')
+model = SVR(kernel='linear')  # max = 79.18%
+# model = SVR(kernel='rbf')  # max = 63.01%
 observation_history = pd.DataFrame(columns=['x0', 'x1', 'x2'])
 good_action_history = pd.DataFrame(columns=['y0'])
 
@@ -20,42 +24,47 @@ def make_action(action_raw):
     return np.array([action_raw], dtype=np.float32)
 
 
-while len(observation_history) < 50:
-    print("Generating random training samples")
+print("Generating random training samples")
+while len(observation_history) < 500: #500
     observation = env.reset()
     reward = False  # reward = -8
     for frame in range(200):
         previous_reward = reward
         env.render()
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(make_action(action))
+        action = make_action(env.action_space.sample())
+        observation, reward, done, info = env.step(action)
         observation = observation.flatten() * np.array([1, 1, 1 / 8])  # Normalize the input variables
         if previous_reward and reward > previous_reward:
             observation_history.loc[len(observation_history)] = observation
-            good_action_history.loc[len(good_action_history)] = action
+            good_action_history.loc[len(good_action_history)] = action.item()
         if done:
             break
 
-for trial in range(10):
+for trial in range(20):
     print(f'Starting trial #{trial+1}')
     observation = env.reset()
     reward = False  # reward = -8
-    model.fit(observation_history, good_action_history.values.ravel())
-    score = model.score(observation_history, good_action_history.values.ravel())
+    model.fit(observation_history, good_action_history.values.ravel().tolist())
+    score = model.score(observation_history, good_action_history.values.ravel().tolist())
     print(f'Model score: {round(score*100, 2)}%, with {len(observation_history)} training samples.')
-    for frame in range(200):
+    for frame in range(201):
         previous_reward = reward
         ###
         env.render()
-        action = model.predict(observation.reshape(1, -1))
-        observation, reward, done, info = env.step(make_action(action))
+        action = make_action(model.predict(observation.reshape(1, -1)))
+        observation, reward, done, info = env.step(action)
         observation = observation.flatten() * np.array([1, 1, 1 / 8])  # Normalize the input variables
         if previous_reward and reward > previous_reward:
             observation_history.loc[len(observation_history)] = observation
-            good_action_history.loc[len(good_action_history)] = action
+            good_action_history.loc[len(good_action_history)] = action.item()
         if done:
-            print(f'Finished trial #{trial+1} after {frame} frames')
+            if frame == 199:
+                print(f'Finished trial #{trial+1} after {frame+1} frames')
+            else:
+                print(f'Model finished early in trial #{trial+1} after only {frame+1} frames')
             break
+
+
 
 # print(f'observation: {observation}')
 # print(f'reward: {reward}')
